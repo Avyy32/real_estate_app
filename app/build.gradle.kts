4b80888891055d43370229aa3b59b1b89321379f
aryangupta36 <aryangupta36@gmail.com>
2026-04-28 11:44:16 +0530

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")   // ✅ ADD THIS
 }
 
 android {
@@ -39,6 +40,7 @@ dependencies {
     implementation(libs.osmdroid)
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
